97f74a593e66e76095b0a146ac280fe4a9619846
HaiTS <haits@vega.com.vn>
2020-06-11 18:24:42 +0700

    pug

--- resources/assets/js/views/components/App.js
+++ resources/assets/js/views/components/App.js
@@ -7,24 +7,15 @@ import i18n from "@/locale/i18n";
 
 class App extends React.Component {
     render() {
-        console.log("App");
-        return (
-            <div className="container">
-                <h1 className="row justify-content-center">
-                    {i18n.t("Welcome to React")}
-                </h1>
-                <div className="row justify-content-center">
-                    <div className="col-md-8">
-                        <div className="card">
-                            <div className="card-header">Example Component</div>
-                            <div className="card-body">
-                                I&apos;m an example component!
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        );
+        return pug`
+          div.container
+            h1.row.justify-content-center #{i18n.t('Welcome to React')}
+            div.row.justify-content-center
+              div.col-md-8
+                div.card
+                  div.card-header Example Component
+                  div.card-body I'm an example component!
+            `;
     }
 }
 
