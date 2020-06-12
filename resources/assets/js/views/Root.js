c7ed92bbee283818a8fd19aa0b697185359c6a3c
HaiTS <haits@vega.com.vn>
2020-06-12 09:43:36 +0700

    network

--- resources/assets/js/views/Root.js
+++ resources/assets/js/views/Root.js
@@ -7,11 +7,51 @@ import App from "@/views/components/App";
 import configureStore from "@/state/store/configureStore";
 import i18n from "@/locale/i18n";
 import MultipleLanguage from "@/views/components/MultipleLanguage";
+import NetworkDisconnect from "@/views/components/NetworkDisconnect";
 
 const store = configureStore();
 
 class Root extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            networkDisconnect: false,
+        };
+    }
+
+    componentWillMount() {
+        window.addEventListener("offline", this._checkNetworkConnection, false);
+        window.addEventListener("online", this._checkNetworkConnection, false);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener(
+            "offline",
+            this._checkNetworkConnection,
+            false
+        );
+        window.removeEventListener(
+            "online",
+            this._checkNetworkConnection,
+            false
+        );
+    }
+
+    _checkNetworkConnection = (e) => {
+        this.setState({
+            networkDisconnect: e.type === "offline",
+        });
+
+        if (!this.state.networkDisconnect) {
+            window.location.reload();
+        }
+    };
+
     render() {
+        let { networkDisconnect } = this.state;
+        if (networkDisconnect) {
+            return <NetworkDisconnect />;
+        }
         return (
             <I18nextProvider i18n={i18n}>
                 <Provider store={store}>
---
+++ resources/assets/js/views/components/NetworkDisconnect.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import i18n from "@/locale/i18n";
+
+class NetworkDisconnect extends React.Component {
+    render() {
+        return (
+            <div className="row justify-content-center">
+                <h1>{i18n.t("No internet")}</h1>
+            </div>
+        );
+    }
+}
+
+export default NetworkDisconnect;
