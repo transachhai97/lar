ea64ec795f6f67e8f000205283d6be003a5ed398
HaiTS <haits@vega.com.vn>
2020-06-17 11:30:17 +0700

    route laravel

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name("welcome");
 
 Route::get('/spa/{any?}', function () {
     return view('spa');
-})->where(['any', '.*']);
+})->where(['any', '.*'])->name("spa");
